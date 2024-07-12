465455e924f4900ba745933b5103dca6d10e4770
kenya <kenya@DESKTOP-0HQ4R98.lan>
2024-07-11 21:21:48 -0500

    Edible

---
+++ EdibleTest/src/Edible.java
@@ -0,0 +1,14 @@
+/*Objective: Create an interface and classes that implement the behavior
+ * specified in an interface.
+ * Algorithm: Each object in an array is checked if they are an instance of the Edible interface and prints a String.
+ * Input and Output: The output is the series of strings from the objects indicating if they are edible or not.
+ * Created by: Kenya Milo
+ * Date: 06/27/2024
+ * Version java 20.0.2
+ */
+//Interface defining an Edible object with hoToEat method
+public interface Edible {
+
+		public abstract String howToEat();
+		
+}
